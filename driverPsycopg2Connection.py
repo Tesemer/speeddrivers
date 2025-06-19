0c2669f1d42d596929c904df609d5186116bdb0d
Michail <michail.magazov@uni-ulm.de>
2025-06-19 18:43:17 +0200

    feat: instead of Create do Select to prevent errors

--- driverPsycopg2Connection.py
+++ driverPsycopg2Connection.py
@@ -7,22 +7,12 @@ try:
         host = "localhost",
         database = "python_driver_db",
         user = "postgres",
-        password = "N*63fza*E5gUrayE",
+        password = "Pelmeni",
         port = 5432
     )
     cursor = conn.cursor()
 
-    create_script = '''CREATE TABLE datatable (id SERIAL PRIMARY KEY, 
-                                               field0 VARCHAR(100), 
-                                               field1 VARCHAR(100), 
-                                               field2 VARCHAR(100), 
-                                               field3 VARCHAR(100), 
-                                               field4 VARCHAR(100), 
-                                               field5 VARCHAR(100), 
-                                               field6 VARCHAR(100), 
-                                               field7 VARCHAR(100), 
-                                               field8 VARCHAR(100), 
-                                               field9 VARCHAR(100));'''
+    create_script = '''SELECT * FROM "datatable";'''
     cursor.execute(create_script)
     conn.commit()
 except Exception as error:
