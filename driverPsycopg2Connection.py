36b91174eb871c5e1965540347ec7b0b8fa6e484
Eric Zahn <eric.zahn@uni-ulm.de>
2025-06-19 18:29:46 +0200

    feat: postgresdb connection established lolollo

--- driverPsycopg2Connection.py
+++ driverPsycopg2Connection.py
@@ -5,19 +5,26 @@ cursor = None
 try:
     conn = psycopg2.connect(
         host = "localhost",
-        database = "bpmn",
+        database = "python_driver_db",
         user = "postgres",
-        password = "Pelmeni",
+        password = "N*63fza*E5gUrayE",
         port = 5432
     )
     cursor = conn.cursor()
 
-    create_script = '''SELECT * FROM "Diagram"'''
+    create_script = '''CREATE TABLE datatable (id SERIAL PRIMARY KEY, 
+                                               field0 VARCHAR(100), 
+                                               field1 VARCHAR(100), 
+                                               field2 VARCHAR(100), 
+                                               field3 VARCHAR(100), 
+                                               field4 VARCHAR(100), 
+                                               field5 VARCHAR(100), 
+                                               field6 VARCHAR(100), 
+                                               field7 VARCHAR(100), 
+                                               field8 VARCHAR(100), 
+                                               field9 VARCHAR(100));'''
     cursor.execute(create_script)
-    diagrams = cursor.fetchall()
-    for diagram in diagrams:
-        print(diagram)
-
+    conn.commit()
 except Exception as error:
     print(error)
 finally:
